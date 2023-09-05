68bb659fda267eebec1ca960b0c4b8baa9b217d2
Andersson <89083903+anderssonslv@users.noreply.github.com>
2023-09-05 15:57:53 -0300

    GUI em testes, e modificação no cache_simulator
    Adição de Interface Gráfica para coleta dos dados da cache, e plotagem de gráfico dos resultados.
    Há como utilizar direto por linha de comando onde não abre a Interface, mas caso não tenha argumentos o programa abre a GUI para inserção dos dados.

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,45 +1,57 @@
 import sys
 from openBinary import openBinary
 from cache import Cache
+from interface import *
 
 # Argumentos definidos por padrão para testes
+"""
 sys.argv.append(256)
 sys.argv.append(4)
-sys.argv.append(2)
-sys.argv.append('f')
+sys.argv.append(4)
+sys.argv.append('r')
 sys.argv.append(1)
-sys.argv.append('bin_100.bin')
-
+sys.argv.append('vortex.in.sem.persons.bin')
+"""
 def main():
-	if (len(sys.argv) != 7):
-		print("Numero de argumentos incorreto. Utilize:")
-		print("python cache_simulator.py <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada")
-		exit(1)
+	values = []
+	C = Cache
+	if len(sys.argv) > 1: # caso tenha argumentos roda sem GUI
+		if (len(sys.argv) != 7):
+			print("Numero de argumentos incorreto. Utilize:")
+			print("python cache_simulator.py <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada")
+			exit(1)
+		else:
+			nsets = sys.argv[1]
+			bsize = sys.argv[2]
+			assoc = sys.argv[3]
+			subst = sys.argv[4]
+			flagOut = sys.argv[5]
+			inputFile = sys.argv[6]
+	else: # sem argumentos coleta os dados com a GUI
+		GUI = Interface()
+		values = GUI.parameters()
+		nsets = int(values[0])
+		bsize = int(values[1])
+		assoc = int(values[2])
+		subst = values[3]
+		flagOut = int(values[4])
+		inputFile = values[5]
 	
-	nsets = sys.argv[1]
-	bsize = sys.argv[2]
-	assoc = sys.argv[3]
-	subst = sys.argv[4]
-	flagOut = sys.argv[5]
-	arquivoEntrada = sys.argv[6]
-
-	C = Cache(nsets,bsize,assoc,subst,flagOut,arquivoEntrada)
-
-	print("Argumentos passados")
-	print("nsets =", nsets)
-	print("bsize =", bsize)
-	print("assoc =", assoc)
-	print("subst =", subst)
-	print("flagOut =", flagOut)
-	print("arquivo =", arquivoEntrada)
+	C = Cache(nsets,bsize,assoc,subst,flagOut,inputFile)
 
 	# Seu codigo vai aqui
-	addresses = openBinary(arquivoEntrada)
-
+	addresses = openBinary(inputFile)
 	for end in addresses:
 		C.simulate_cache_access(int(end))
-
 	C.imprime()
+
+	cacheInfo = f'Cache {nsets} Conjuntos, Tamanho de bloco {bsize} ,Associatividade {assoc}, Algoritmo {subst}'
+	labels = ['Hits','Miss','Misses Compulsórios','Misses capacidade']
+	cacheData = C.getData()
+
+	print(cacheData)
+	G = Graph(labels,cacheData,C.getAcess(),cacheInfo)
+	G.plot()
 
 
 if __name__ == '__main__':
